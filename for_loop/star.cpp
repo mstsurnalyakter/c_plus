51c9c203598966560c17c7b74d7c45e9264f8370
mstsurnalyakter <surnalyms@gmail.com>
2024-12-31 07:06:03 -0700

    Add star pattern printing program using nested loops in star.cpp

---
+++ for_loop/star.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int n = 5;
+for(int i = 1; i<=n;i++){
+    int m = 10;
+    for(int j= 1; j<=m; j++){
+        cout << "*";
+    }
+    cout << "\n";
+}
+
+return 0;
+}
