d3d79c280a7575ac6539c8be673ef278b1d7b725
mstsurnalyakter <surnalyms@gmail.com>
2024-12-31 03:46:34 -0700

    Add relational operators demonstration in relational.cpp

---
+++ operators/relational.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int a=5, b = 8;
+
+cout << (a<b)<< "\n";
+cout << (a>b) << "\n";
+cout << (a>=b) << "\n";
+cout << (a<=b) << "\n";
+cout << (a>b) << "\n";
+cout << (a!=b) << "\n";
+
+return 0;
+}
