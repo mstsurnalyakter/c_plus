e4f8d04f86d18b78c4cb71fb3364df05127bcea7
mstsurnalyakter <surnalyms@gmail.com>
2024-12-31 01:21:14 -0700

    Add variable3.cpp with demonstration of variable types and I/O operations

---
+++ variable/variable3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int id = 101;
+char star = '*';
+float PI = 3.14;
+bool isSave = true;
+double price = 100.44;
+
+cout << id << "\n";
+cout << sizeof(id) << "\n";
+cout << star << "\n";
+cout << PI << "\n";
+cout << isSave << "\n";
+cout << price << "\n";
+
+return 0;
+}
