7c9221ebc847f6787feb12caf349b2a841fa630e
mstsurnalyakter <surnalyms@gmail.com>
2024-12-31 08:15:23 -0700

    Add program to print a triangular pattern of incrementing numbers in p10.cpp

---
+++ for_loop/patterns/p10.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int n =4;
+for(int i =0; i<n; i++){
+        int num = 1;
+    for(int j=0; j<=i; j++){
+       cout << num;
+        num ++;
+    }
+    cout << "\n";
+}
+return 0;
+}
