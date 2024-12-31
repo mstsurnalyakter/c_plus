106ffe23e2ecfc6776f252f3ab92ff6049ff9df3
mstsurnalyakter <surnalyms@gmail.com>
2024-12-31 06:07:00 -0700

    Add character case identification demonstration in case.cpp

---
+++ if_else/case.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+char ch ;
+cout << "Enter a character: ";
+cin >> ch;
+if(ch <= 65 && ch >= 90){
+    cout << "Uppercase";
+}else{
+cout << "Lowercase";
+}
+return 0;
+}
