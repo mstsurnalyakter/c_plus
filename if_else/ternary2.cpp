69d5b54e758e2704044e85a857904e70193331be
mstsurnalyakter <surnalyms@gmail.com>
2024-12-31 06:16:16 -0700

    Add ternary operator demonstration in ternary2.cpp

---
+++ if_else/ternary2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int n= 50;
+cout << (n >= 0 ? "Positive" : "Negative" ) << "\n";
+cout <<  (n%2==0 ? "Even" : "Odd") << "\n";
+return 0;
+}
