fa78993c0480b6945de16d79f9f98b4b272738db
mstsurnalyakter <surnalyms@gmail.com>
2024-12-31 03:14:30 -0700

    Add arithmetic operators demonstration in arithmetic.cpp

---
+++ operators/arithmetic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int a=5, b = 8;
+int sum = a + b;
+cout << "Sum=" << (a+b) << "\n";
+cout << sum << "\n";
+cout << "Substracti = " << (a - b) << "\n";
+cout << "Multiple = " << (a*b) << "\n";
+cout << "Division = " << (a/b) << "\n";
+cout << "Modulo = " << (a%b) << "\n";
+
+return 0;
+}
