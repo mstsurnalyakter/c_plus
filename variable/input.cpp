2f152c26d04a901615524a64dc89220bfab753a6
mstsurnalyakter <surnalyms@gmail.com>
2024-12-31 03:03:02 -0700

    Add input.cpp to demonstrate basic I/O operations for user input

---
+++ variable/input.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int id;
+int phone;
+cout << "Enter your id: ";
+cin >> id;
+
+cout << "Enter you phone number:";
+cin >> phone;
+
+cout << "Your id is: " << id << "\n";
+cout << "Your Phone number is: " << phone << "\n";
+
+return 0;
+
+}
