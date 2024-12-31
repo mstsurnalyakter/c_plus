44d8728b8b0a039b1154174dd543c89c1fa63b72
mstsurnalyakter <surnalyms@gmail.com>
2024-12-31 02:52:34 -0700

    Add typeCasting.cpp to demonstrate type casting in C++

---
+++ variable/typeCasting.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+char star = 'a';
+double price = 100.88;
+
+//type casting Big value --> small value;
+int newPrice = (int)price;
+int value = star;
+
+cout << value << "\n";
+cout << newPrice << "\n";
+
+
+
+
+
+
+return 0;
+}
